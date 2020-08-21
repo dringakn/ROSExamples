fbe954062824c90d6a8a6212e9ae47d2eb7ee87b
Ahmad Kamal Nasir <dringakn@gmail.com>
2020-08-21 11:10:50 +0100

    feat: Add color to the PC

--- src/example_pcl_cloud_viewer.cpp
+++ src/example_pcl_cloud_viewer.cpp
@@ -1,11 +1,13 @@
 #include <bits/stdc++.h>                     // Standard C++ libraries
 #include <pcl/point_cloud.h>                 // PCL
 #include <pcl/visualization/cloud_viewer.h>  // PC Visulizer
-#include <ros/ros.h>                         //ROS
+#include <pcl/visualization/pcl_visualizer.h>
+#include <ros/ros.h>  //ROS
+
+using namespace std;
 
 // Use Intensity to store data.
-using namespace std;
-typedef pcl::PointXYZI Point3D;               // Points types
+typedef pcl::PointXYZRGB Point3D;             // Points types
 typedef pcl::PointCloud<Point3D> PointCloud;  // PointCloud
 
 /**
@@ -54,8 +56,7 @@ int main(int argc, char* argv[]) {
   std::mt19937 rng(0);  // Mersenne Twister RNG
   std::uniform_real_distribution<float> dist(-10, 10);
 
-  // Point cloud visulizer
-  pcl::visualization::CloudViewer viewer("PC Viewer");
+  // Point cloud
   PointCloud::Ptr cloud(new PointCloud());             // Create a pointer to PC
   cloud->height = 100;                                 // Organized point cloud
   cloud->width = 100;                                  // Cloud width
@@ -67,18 +68,40 @@ int main(int argc, char* argv[]) {
     pt.x = dist(rng);
     pt.y = dist(rng);
     pt.z = dist(rng);
-    pt.intensity = 64;
+    // pt.intensity = 64;
+    pt.g = 255;
     // uint32_t rgb = (static_cast<uint32_t>(255) << 16 |  // R G B
     //                 static_cast<uint32_t>(15) << 8 |
     //                 static_cast<uint32_t>(15));
     // pt.rgb = *reinterpret_cast<float*>(&rgb);
   }
 
-  viewer.showCloud(cloud);  // Display the point cloud
+  // Point cloud visulizer
+  // Method 1
+  // pcl::visualization::CloudViewer viewer("PC Viewer");
+  // viewer.showCloud(cloud);  // Display the point cloud
+
+  // Method 2
+  pcl::visualization::PCLVisualizer visulizer("PC Visulizer");
+  visulizer.setBackgroundColor(0, 0, 0);
+  visulizer.addCoordinateSystem(10);
+  visulizer.addPointCloud<Point3D>(cloud);
+  Point3D center;
+  visulizer.addSphere<Point3D>(center, 1);
+  pcl::ModelCoefficients coeffs;
+  coeffs.values.push_back(0.0);
+  coeffs.values.push_back(1.0);
+  coeffs.values.push_back(0.0);
+  coeffs.values.push_back(0.0);
+  visulizer.addPlane(coeffs, 2, 2, 0);
+  visulizer.initCameraParameters();
+
+  // Perodic code
   ros::Rate loop_rate(10);  // Node update rate
   while (ros::ok()) {
-    if (viewer.wasStopped(50))  // Check if the window is closed
-      ros::shutdown();          // close ros
+    visulizer.spinOnce(1, true);
+    if (visulizer.wasStopped())  // Check if the window is closed
+      ros::shutdown();           // close ros
     ros::spinOnce();
     loop_rate.sleep();
   }
