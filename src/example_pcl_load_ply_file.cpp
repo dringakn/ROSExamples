6296855d3b086b22129ee10cb2b59487d4bcf29d
Ahmad Kamal Nasir <ahmad.nasir@telematik-zentrum.de>
2021-05-25 23:03:22 +0200

    feat: parametrize pointcloud file name

--- src/example_pcl_load_ply_file.cpp
+++ src/example_pcl_load_ply_file.cpp
@@ -12,13 +12,28 @@
 
 int main(int argc, char* argv[]) {
   ros::init(argc, argv, "example_pcl_load_ply_file");
-  ros::NodeHandle nh;
+  ros::NodeHandle nh("~");
+
+  std::string map_file_name;
+  if (!nh.param<std::string>("map_file_name", map_file_name, "/home/workshop/LUMS1.ply"))
+    nh.setParam("map_file_name", map_file_name);
+  ROS_INFO("Map file: %s", map_file_name.c_str());
+
   ros::Publisher pub =
       nh.advertise<sensor_msgs::PointCloud2>("/pointcloud", 1000, true);
+ 
   pcl::PCLPointCloud2* pc = new pcl::PCLPointCloud2;
-  if (pcl::io::loadPLYFile("/home/workshop/LUMS1.ply", *pc) == -1) {
-    ROS_INFO("Pointcloud file not loaded");
+ 
+  if (pcl::io::loadPLYFile(map_file_name.c_str(), *pc) == -1) {
+    ROS_INFO("Pointcloud file (%s) not loaded", map_file_name.c_str());
     return -1;
+  }else{
+    ROS_INFO("Loaded %dx%d points from %s", pc->width, pc->height, map_file_name.c_str());
+    sensor_msgs::PointCloud2 msg;
+    pcl_conversions::fromPCL(*pc, msg);
+    msg.header.stamp = ros::Time::now();
+    msg.header.frame_id = "map";
+    pub.publish(msg);
   }
 
   ros::Rate rate(1);
