43111d4d5eb31ec4d1b1716dbbff5d91bdaa5457
Ahmad Kamal Nasir <ahmad.nasir@telematik-zentrum.de>
2022-05-01 18:11:11 +0200

    chore: formated code.

--- src/example_path_planning_astar.cpp
+++ src/example_path_planning_astar.cpp
@@ -3,7 +3,7 @@
     Email: dringakn@gmail.com
     Description:
       Test the AStar algorithm.
-      For testing an occupancy grid map is required. 
+      For testing an occupancy grid map is required.
       If there doesn't exist any, Use the example_occupancy_grid_map to create and publish one.
       If it already exist then use the map_server to load and publish the map on a ROS topic.
 
@@ -13,7 +13,7 @@
       on a latched topic and a transform between world->map frame. We can write
       a node which can subscribe to the "map" topic.
 
-      rosrun ros_examples example_occupancy_grid_map_service_request topic file.txt 
+      rosrun ros_examples example_occupancy_grid_map_service_request topic file.txt
       The "example_occupancy_grid_map_service_request" node can be used
       to obtain the map from the specified topic through a service request and
       save it to a ACSII text file. service (to be implemented) to obtain the
@@ -44,69 +44,75 @@ AStar g;
 ros::Publisher path_pub;
 
 bool frame2map(const tf::TransformListener &li, geometry_msgs::PointStamped &pt,
-               geometry_msgs::PointStamped &pt_m) {
+               geometry_msgs::PointStamped &pt_m)
+{
   // target frame, source frame
-  if (li.canTransform("map", pt.header.frame_id, ros::Time(0))) {
+  if (li.canTransform("map", pt.header.frame_id, ros::Time(0)))
+  {
     li.transformPoint("map", pt, pt_m);
-    std::cout << pt_m.header.frame_id << ": " << pt_m.point.x << ","
-              << pt_m.point.y << "," << pt_m.point.z << std::endl;
+    std::cout << pt_m.header.frame_id << ": " << pt.point.x << "," << pt.point.y << "," << pt.point.z << " -> " << pt_m.point.x << "," << pt_m.point.y << "," << pt_m.point.z << std::endl;
     return true;
-  } else {
+  }
+  else
+  {
     ROS_INFO("Transform is not available");
     return false;
   }
 }
 
-void map_cb(const nav_msgs::OccupancyGrid::ConstPtr &msg) {
+void map_cb(const nav_msgs::OccupancyGrid::ConstPtr &msg)
+{
   ROS_INFO("Map: %d x %d @ %0.3f m/px", msg->info.width, msg->info.height, msg->info.resolution);
   g.setMap(msg);
 }
 
 void initpose_cb(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr &msg,
-                 const tf::TransformListener &li) {
+                 const tf::TransformListener &li)
+{
   geometry_msgs::PointStamped pt, pt_m;
   pt.header = msg->header;
   pt.header.stamp = ros::Time();
   pt.point = msg->pose.pose.position;
-  if (frame2map(li, pt, pt_m)) 
+  if (frame2map(li, pt, pt_m))
     g.setStartPoint(pt_m);
 }
 
 void goalpose_cb(const geometry_msgs::PoseStamped::ConstPtr &msg,
-                 const tf::TransformListener &li) {
+                 const tf::TransformListener &li)
+{
   geometry_msgs::PointStamped pt, pt_m;
   pt.header = msg->header;
   pt.header.stamp = ros::Time();
   pt.point = msg->pose.position;
-  if (frame2map(li, pt, pt_m)) {
+  if (frame2map(li, pt, pt_m))
+  {
     g.setGoalPoint(pt_m);
     int pathSteps = g.shortestPath();
-    if (pathSteps != 0) {
+    if (pathSteps != 0)
+    {
       cout << "Path Found, Steps:" << pathSteps << endl;
       path_pub.publish(g.getPath());
-    } else {
-      cout << "Path Not Found" << endl;
+    }
+    else
+    {
+      cout << "Path 'Not' Found, Steps:" << pathSteps << endl;
     }
   }
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   ros::init(argc, argv, "astar");
   ros::NodeHandle nh;
   tf::TransformListener li(ros::Duration(10));
 
   path_pub = nh.advertise<nav_msgs::Path>("path", 1, true);
 
-  ros::Subscriber initPose_sub =
-      nh.subscribe<geometry_msgs::PoseWithCovarianceStamped>(
-          "initialpose", 1, boost::bind(&initpose_cb, _1, boost::ref(li)));
+  ros::Subscriber initPose_sub = nh.subscribe<geometry_msgs::PoseWithCovarianceStamped>("initialpose", 1, boost::bind(&initpose_cb, _1, boost::ref(li)));
 
-  ros::Subscriber goalPose_sub = nh.subscribe<geometry_msgs::PoseStamped>(
-      "move_base_simple/goal", 1,
-      boost::bind(&goalpose_cb, _1, boost::ref(li)));
+  ros::Subscriber goalPose_sub = nh.subscribe<geometry_msgs::PoseStamped>("move_base_simple/goal", 1, boost::bind(&goalpose_cb, _1, boost::ref(li)));
 
-  ros::Subscriber map_sub =
-      nh.subscribe<nav_msgs::OccupancyGrid>("map", 1, map_cb);
+  ros::Subscriber map_sub = nh.subscribe<nav_msgs::OccupancyGrid>("map", 1, map_cb);
 
   ros::spin();
   return 0;
