8f3511f104977dbe79c3a9e8215bb88dfd3e3184
Ahmad Kamal Nasir <ahmad.nasir@telematik-zentrum.de>
2022-05-01 18:15:50 +0200

    fix: invalid width height calculations.

--- src/AStar.cpp
+++ src/AStar.cpp
@@ -21,7 +21,8 @@ AStar::AStar() : gcost{
 AStar::~AStar() {}
 
 /**
- * @brief Set the start point for the path. The point is assumed to be in the map frame.
+ * @brief Set the start point for the path. The point is assumed to be in the map frame. The
+ *        input point is converted to grid cell location.
  *
  * @param s geometry_msgs::PointStamped
  */
@@ -31,10 +32,12 @@ void AStar::setStartPoint(geometry_msgs::PointStamped &s)
   start.point.x = floor(s.point.x / map->info.resolution);
   start.point.y = floor(s.point.y / map->info.resolution);
   start.point.z = floor(s.point.z / map->info.resolution);
+  // std::cout << start.point << std::endl;
 }
 
 /**
- * @brief set the end point for the path. The point is assumed to be in the map frame.
+ * @brief Set the end point for the path. The point is assumed to be in the map frame.The
+ *        input point is converted to grid cell location.
  *
  * @param g geometry_msgs::PointStamped
  */
@@ -44,6 +47,7 @@ void AStar::setGoalPoint(geometry_msgs::PointStamped &g)
   goal.point.x = floor(g.point.x / map->info.resolution);
   goal.point.y = floor(g.point.y / map->info.resolution);
   goal.point.z = floor(g.point.z / map->info.resolution);
+  // std::cout << goal.point << std::endl;
 }
 
 /**
@@ -82,19 +86,24 @@ int AStar::shortestPath()
   vector<int> parent(n, -1);                               // Set the parent value to each of the grid cell to be unknown (-1)
 
   // Return if the start point is outside the map
-  if (start.point.x < 0 || start.point.x >= width || start.point.y < 0 ||
-      start.point.y >= height)
+  if ((start.point.x < 0) || (start.point.x >= width) || (start.point.y < 0) || (start.point.y >= height))
+  {
+    ROS_INFO("AStar start point is outside the map.");
     return 0;
+  }
 
   // Return if the goal point is outside the map
-  if (goal.point.x < 0 || goal.point.x >= width || goal.point.y < 0 ||
-      goal.point.y >= height)
+  if ((goal.point.x < 0) || (goal.point.x >= width) || (goal.point.y < 0) || (goal.point.y >= height))
+  {
+    ROS_INFO("AStar goal point is outside the map.");
     return 0;
+  }
 
-  int sidx = start.point.x + start.point.y * height;
+  // start point 1D array index = col_index + (row_index x total_columns)
+  int sidx = start.point.x + (start.point.y * width);
   cost[sidx] = 0;
 
-  // Add the start point to the pirority queue
+  // Add the start point to the pirority queue (col, row)
   point pt(start.point.x, start.point.y, cost[sidx]);
   pq.push(pt);
 
@@ -105,11 +114,11 @@ int AStar::shortestPath()
     pq.pop();
 
     // If the current cell is the goal, stop searching
-    if (pt.x == goal.point.x && pt.y == goal.point.y)
+    if ((pt.x == goal.point.x) && (pt.y == goal.point.y))
       break;
 
     /*
-      Find the neighbouring cells (3x3)
+      Find the neighbouring cells (3x3): r=row=y-axis, c=col=x-axis
       Generating all the 8 successor of this cell
 
           NW[0]   N[1]     NE[2]
@@ -132,35 +141,35 @@ int AStar::shortestPath()
       SE--> South-East  (r+1, c+1) --> index:8
       SW--> South-West  (r+1, c-1) --> index:6
     */
-    for (int r = -1; r <= 1; r++)
-      for (int c = -1; c <= 1; c++)
+    for (int r = -1; r <= 1; r++)   // y-axis
+      for (int c = -1; c <= 1; c++) // x-axis
       {
 
         // Skip if it's center cell
-        if (r == 0 && c == 0)
+        if ((r == 0) && (c == 0))
           continue;
 
         // Skip if it's beyond map boundary
-        int x = c + pt.x;
         int y = r + pt.y;
-        if (x < 0 || y < 0 || x >= width || y >= height)
+        int x = c + pt.x;
+        if ((x < 0) || (y < 0) || (x >= width) || (y >= height))
           continue;
 
         // Skip if it's a non-reachable cell, 0=free, -1=unknown, 100=occupied
-        int nidx = x + y * height;
-        if (map->data[nidx] == 100)
+        int nidx = x + y * width;
+        if ((map->data[nidx] == 100) || (map->data[nidx] == -1))
           continue;
 
         // Weighted cost in all direction, Diagonal are most costlier
-        // newcost = previouscost + motioncost (Euclidean distance) + heuristic
+        // new cost = previous cost + motioncost (Euclidean distance) + heuristic
         // motioncast: cost to move from current to the neighbour; sqrt(r * r + c * c) or gcost(r,c)
         // heuristic: approximate cost to move from neighbour to the target
         // Note: The Euclidean distance for diagonal is more than the longitudinal/lateral
         //       <<< A 3x3 matrix can also be used for custom cost >>>
         //       The difference between Dijkstra and AStar is only the second term (heuristic).
-        int pidx = pt.x + pt.y * height;
-        float ncost = pt.cost + gcost[r+1][c+1] + sqrt(pow(x - goal.point.x, 2) + pow(y - goal.point.y, 2));
-        if (ncost < cost[nidx]) // Add the cell to the queue if new cost is smaller than prior cost
+        float ncost = pt.cost + gcost[r + 1][c + 1] + sqrt(pow(x - goal.point.x, 2) + pow(y - goal.point.y, 2));
+        int pidx = pt.x + pt.y * width;
+        if (ncost < cost[nidx])         // Add the cell to the queue if new cost is smaller than prior cost
         {
           // Update the cell cost, save it's parent, and add it to the queue
           cost[nidx] = ncost;      // Update the cost value
@@ -173,9 +182,9 @@ int AStar::shortestPath()
 
   } // While the queue is not empty
 
-  // the path is found if it's cost has been changed
-  int gidx = goal.point.x + goal.point.y * height;
-  if (cost[gidx] < INF)
+  
+  int gidx = goal.point.x + goal.point.y * width;
+  if (cost[gidx] < INF) // the path is found if it's cost has been changed
   {
     path.header.seq = path.header.seq + 1;
     path.header.stamp = ros::Time();
@@ -194,6 +203,7 @@ int AStar::shortestPath()
       path.poses.push_back(pos);
       idx = parent[idx];
       steps++;
+      // std::cout << idx << " -> " << pos.pose.position.x << ", " << pos.pose.position.y << std::endl;
     }
 
     cout << "Explored Cells:" << exploredCells << endl;
