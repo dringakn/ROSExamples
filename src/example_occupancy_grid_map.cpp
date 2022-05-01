8ee7b05c9a2c95aa44037a4c460efdfcbfb7447c
Ahmad Kamal Nasir <ahmad.nasir@telematik-zentrum.de>
2022-05-01 18:09:26 +0200

    chore: formeatted comments

--- src/example_occupancy_grid_map.cpp
+++ src/example_occupancy_grid_map.cpp
@@ -24,7 +24,8 @@
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   ros::init(argc, argv, "example_occupancy_grid_map");
   ros::NodeHandle n;
   ros::Publisher map_pub = n.advertise<nav_msgs::OccupancyGrid>("map", 1, true);
@@ -40,13 +41,17 @@ int main(int argc, char *argv[]) {
   map.info.resolution = (argc > 3) ? atof(argv[3]) : 1;
   ROS_INFO("Map: %d x %d @ %0.3f", map.info.width, map.info.height,
            map.info.resolution);
-  if (argc > 4 && atoi(argv[4]) == 1) {
+  if (argc > 4 && atoi(argv[4]) == 1)
+  {
     map.info.origin.position.x = rng.uniformInteger(-5, 5);
     map.info.origin.position.y = rng.uniformInteger(-5, 5);
     map.info.origin.position.z = rng.uniformInteger(-5, 5);
-  } else {
+  }
+  else
+  {
     map.info.origin = geometry_msgs::Pose();
   }
+  
   map.data.resize(map.info.width * map.info.height);
   //-1=Unknown, 0=Free, 100=Occupied
   for (size_t idx = 0; idx < map.data.size(); idx++)
@@ -55,10 +60,10 @@ int main(int argc, char *argv[]) {
   map_pub.publish(map);
 
   tf::TransformBroadcaster br;
-  tf::Transform t;
+  while (ros::ok())
+  {
   t.setOrigin(tf::Vector3(0, 0, 0));
-  t.setRotation(tf::createQuaternionFromRPY(0, 0, 0));
-
+    br.sendTransform(tf::StampedTransform(t, ros::Time::now(), "world", map.header.frame_id));
   ros::Rate rate(10);
   while (ros::ok()) {
     // parent, child: broadcast map to world
