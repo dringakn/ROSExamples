951472ff75b866efa2a17d8f70b531c04b8aaf17
Ahmad Kamal Nasir <ahmad.nasir@telematik-zentrum.de>
2022-11-15 22:15:08 +0100

    feat: read numbers from text file

--- src/example_read_numbers_from_text_file.cpp
+++ src/example_read_numbers_from_text_file.cpp
@@ -9,15 +9,23 @@ bool readPoseArrayFile(std::string fname, geometry_msgs::PoseArray &data)
     std::string line;
     while (getline(file, line)) // iostream
     {
-        std::istringstream ss(line); // sstream
-        geometry_msgs::Pose p;
-        ss >> p.position.x >> p.position.y >> p.position.x >> p.orientation.x >> p.orientation.y >> p.orientation.z >> p.orientation.w;
-        data.poses.push_back(p);
-        result = true;
+        if (line.size()) // Remove empty line
+        {
+            if (line[0] != '#') // Skip comment line starting with '#'
+            {
+                std::istringstream ss(line); // sstream
+                geometry_msgs::Pose p;
+                // Line format: x y z qx qy qz qw
+                // Note: if q(x,y,z,w) are missing, by default filled with zero
+                ss >> p.position.x >> p.position.y >> p.position.z >> p.orientation.x >> p.orientation.y >> p.orientation.z >> p.orientation.w;
+                data.poses.push_back(p);
+                result = true;
+            }
+        }
     }
-
     return result;
 }
+
 int main(int argc, char *argv[])
 {
     std::string fname = "/home/ahmad/sample.txt";
